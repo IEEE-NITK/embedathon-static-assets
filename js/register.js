a1373df5bd8ae3a53bc0453cc4e5a70bf19c5f25
nishant-nayak <nishant-nayak@users.noreply.github.com>
2022-11-09 18:36:55 +0000

    deploy: be58e82d048fd124800f76ff5ed75b8ce26069f6

--- js/register.js
+++ js/register.js
@@ -1,13 +1,21 @@
-function checkPassword(form) {
-    p1 = form.password.value;
-    p2 = form.confirmation.value;
+function checkFormValidity(form) {
+    password = form.password.value;
+    confirmation = form.confirmation.value;
+    phone = form.phone.value.toString();
 
-    if (p1 != p2 || p1 == '' || p2 == '') {
+    if (phone.toString().length < 10) {
+        form.phone.setCustomValidity("Phone number must be 10 digits");
+        return false;
+    }
+    form.phone.setCustomValidity('');
+
+    if (password != confirmation || password == '' || confirmation == '') {
         form.password.setCustomValidity("Passwords don't match");
         form.confirmation.setCustomValidity("Passwords don't match");
         return false;
     }
     form.password.setCustomValidity('');
     form.confirmation.setCustomValidity('');
+
     return true;
 }
